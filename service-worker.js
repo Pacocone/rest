28bb5ef1d93753b3aed82686439313469602f8a1
Pacocone <pacoconesa@vaquelia.com>
2025-07-31 19:56:49 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='rt-v1363';const PRECACHE=["/rest/", "/rest/index.html", "/rest/styles.css?v=1363", "/rest/script.js?v=1363", "/rest/config.js?v=1363", "/rest/manifest.webmanifest", "/rest/icons/icon-192.png", "/rest/icons/icon-512.png"];
+const CACHE='rt-v1364';const PRECACHE=["/rest/", "/rest/index.html", "/rest/styles.css?v=1364", "/rest/script.js?v=1364", "/rest/config.js?v=1364", "/rest/manifest.webmanifest", "/rest/icons/icon-192.png", "/rest/icons/icon-512.png"];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(PRECACHE)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{const url=new URL(e.request.url);if(url.origin===location.origin){e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return res;})).catch(()=>caches.match('/rest/index.html')));}});
